04c2bfeeca6c62ac66f71482126f8d57cb2d95d2
Kwasi Adu <kwasi.adu@su.org>
2017-11-16 02:12:39 -0800

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule, Provider} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { InitiativeComponent } from './initiative/initiative.component';
+import { InitiativeDetailComponent } from './initiative-detail/initiative-detail.component';
+import { InitiativeService } from './initiative.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitiativeComponent
+    InitiativeComponent,
+    InitiativeDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [InitiativeService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
