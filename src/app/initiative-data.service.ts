97fd472f3597785280d298f3fbaff6cb5d188f3d
Kwasi Adu <kwasi.adu@su.org>
2017-12-22 21:55:29 -0800

    Improved styling of detailed page. Added 'how it all began' story column. Added GGC blurbs.

--- src/app/initiative-data.service.ts
+++ src/app/initiative-data.service.ts
@@ -8,6 +8,53 @@ export class InitiativeDataService {
   TECHS: String[]  = data['tech_focus_options'];
   REGIONS: String[] = data['region_of_impact_options'];
 
+  private blurb = {
+    "security": "Safety of all people from physical and psychological harm, " +
+    "including in virtual worlds; and protection of physical, financial, " +
+    "digital systems.",
+
+    "energy": "Ample, accessible and sustainable energy for the needs of humanity.",
+
+    "environment": "Sustainable and equitable stewardship of Earth's ecosystems for" +
+    " optimal functioning both globally and locally.",
+
+    "food": "Consumption of sufficient, safe, and nutritious food to maintain healthy " +
+    "and active lives for all people at all times.",
+
+    "shelter": "Secure, safe, and sustainable shelter for residence, recreation, and" +
+    " industry for all people at all times.",
+
+    "space": "Safe and equitable use, and stewardship of, space resources and " +
+    "technologies for the benefit of humanity and our future as a multi-planetary " +
+    "species.",
+
+    "water": "Ample and safe water for consumption, sanitation, industry, and recreation " +
+    "for all people at all times.",
+
+    "disaster resilience": "Effective and efficient disaster risk reduction, emergency " +
+    "response, and rehabilitation that saves lives and livelihoods, minimizes economic " +
+    "loss, and builds resilience both globally and locally.",
+
+    "governance": "Equitable participation of all people in formal and societal " +
+    "governance that is in accordance with principles of justice and individual " +
+    "rights, free from discrimination and identity-based prejudices, and able to " +
+    "meet the needs of an exponentially changing world.",
+
+    "health": "Optimal physical and mental health, including access to cost-effective " +
+    "prevention, early diagnosis, and personalized therapy for individuals and " +
+    "communities.",
+
+    "learning": "Access to information and experiences that build knowledge and " +
+    "skills for all people at all stages of their lives for personal fulfillment " +
+    "and benefit to society.",
+
+    "prosperity": "Equitable access to economic and other opportunities for " +
+    "self-fulfillment where all people are free from poverty and able to thrive.",
+};
+
+  public getBlurb (ggc: string) {
+    return this.blurb[ggc.toLowerCase()];
+}
 
   constructor() {
   }
--- src/app/initiative-detail/initiative-detail.component.ts
+++ src/app/initiative-detail/initiative-detail.component.ts
@@ -3,6 +3,9 @@ import { Initiative } from '../initiative';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { InitiativeSearchService } from '../initiative-search.service';
+import {InitiativeDataService} from "../initiative-data.service";
+
+
 
 @Component({
   selector: 'app-initiative-detail',
@@ -11,11 +14,12 @@ import { InitiativeSearchService } from '../initiative-search.service';
   providers: [InitiativeSearchService]
 })
 export class InitiativeDetailComponent implements OnInit {
-
+  ggcBlurb: string;
   @Input() initiative: Initiative;
   constructor(
     private route: ActivatedRoute,
     private initiativeSearchService: InitiativeSearchService,
+    private initiativeDataService: InitiativeDataService,
     private location: Location
   ) {
 
@@ -29,11 +33,12 @@ export class InitiativeDetailComponent implements OnInit {
    * Gets initiative to be displayed and stringify it's array properties
    */
   activate() {
-    const name = this.route.snapshot.params['name'];
+    const name: string = this.route.snapshot.params['name'];
     this.initiative = this.initiativeSearchService.getInitiative(name);
     this.initiative.ggc = this.initiative.ggc_focus.join(", ");
     this.initiative.tech = this.initiative.tech_focus.join(", ");
     this.initiative.region = this.initiative.region_of_impact.join(", ");
+    this.ggcBlurb = this.initiativeDataService.getBlurb(this.initiative.ggc_focus[0])
   }
 
 
